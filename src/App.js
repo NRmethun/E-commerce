c005961fa139c4f4b8f356e12bc7b8875afe64f1
NRmethun <nrmethun@gmail.com>
2021-08-08 09:18:54 +0600

    e-commerce-project with react firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './Component/Header'
+import Home from './Pages/Home'
+import Checkout from './Pages/Checkout';
+import Login from './Component/Login'; 
+import { auth } from "./Firebase/firebase"
+import { useStateValue } from './Pages/ContextApi/StateProvider';
+import { useEffect } from 'react';
+function App() { 
+  const [{user},dispatch ] =useStateValue() ;
+
+  useEffect(()=> {
+        const unsubscribe =  auth.onAuthStateChanged((authUser) => {
+            if(authUser) 
+            {
+              // the user is looged in
+              dispatch({
+                type:"SET_USER",
+                user:authUser,
+              })
+
+            }else {
+              // the user is logged out
+              dispatch({
+                type:"SET_USER",
+                user:null,
+
+              })
+
+            }
+
+          } );
+
+          return () =>{
+            // clean up operation 
+            unsubscribe() ;
+          }
+
+  },[] ) ;
+  console.log("user ==> " ,user) ;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        bismillah
+        <Switch>
+          <Route path="/checkout">
+            <Header /> 
+            <Checkout />
+            <h1>Checkout</h1>
+
+          </Route>
+          <Route path="/login"> 
+             <Login/>
+         
+           
+          </Route>
+          {/* this is default router */}
+          <Route path="/">
+            <Header />
+            <Home />
+
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
